f8dcd7135e2ee60e7c031524ebaf935eeb5ed06c
WangX-Lab <54932820+WangX-Lab@users.noreply.github.com>
2023-09-04 13:43:28 +0800

    Update scPathClus.py

--- scPathClus.py
+++ scPathClus.py
@@ -67,12 +67,12 @@ autoencoder_fit = ae_.autoencoder.fit(
 ## plot the loss curve
 loss_values = autoencoder_fit.history["loss"] 
 epochs  = list(range(1, 51))
-epoch_loss = pd.DataFrame({'epoch': epoch, 'loss': ae_loss})
+epoch_loss = pd.DataFrame({'epoch': epochs, 'loss': loss_value})
 
 plt.plot(epochs, loss_values, 'b', marker='o', linestyle='-')
 plt.title('Loss curve')
 plt.xlabel('Epochs')
-plt.ylabel('Loss')
+plt.ylabel('MSE loss')
 plt.grid(True)
 plt.show()
 
